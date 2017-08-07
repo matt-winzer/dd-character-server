f28b189b573b4b482ab76112252a2e9bd2032b13
matt-winzer <matt.winzer@gmail.com>
2017-08-07 15:57:20 -0600

    created migration for character_item

---
+++ migrations/20170807155616_12_character_item.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('character_item', (table) =>{
+    table.increments();
+    table.integer('character_id').references('character.id').unsigned().onDelete('cascade');
+    table.integer('item_id').references('item.id').unsigned().onDelete('cascade');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('character_item');
+};
