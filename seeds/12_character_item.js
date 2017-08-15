d69b638eebc57b40e1328cf15549dc88d4cc56c7
matt-winzer <matt.winzer@gmail.com>
2017-08-15 17:28:50 -0600

    seed data for ability working

--- seeds/12_character_item.js
+++ seeds/12_character_item.js
@@ -1,17 +1,25 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex.raw('DELETE FROM "character_item"; ALTER SEQUENCE character_item_id_seq RESTART WITH 3;')
+  return knex.raw('DELETE FROM "character_item"; ALTER SEQUENCE character_item_id_seq RESTART WITH 4;')
     .then(function () {
       var character_items = [
         {
           id: 1,
+          quantity: 1,
           character_id: 1,
-          item_id: 8
+          item_id: 87
         },
         {
           id: 2,
+          quantity: 1,
           character_id: 1,
-          item_id: 12
+          item_id: 79
+        },
+        {
+          id: 3,
+          quantity: 3,
+          character_id: 1,
+          item_id: 79
         }
       ];
       return knex('character_item').insert(character_items);
