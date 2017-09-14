132553129bda882d5e6d574aeb8f867c5b5c7f58
matt-winzer <matt.winzer@gmail.com>
2017-09-14 11:34:26 -0600

    fixed connection

--- db/knex.js
+++ db/knex.js
@@ -1,11 +1,11 @@
-const environment = process.env.NODE_ENV || 'development';
-const config = require('../knexfile.js')[environment];
-
 const objection = require('objection');
 const Model = objection.Model;
-const Knex = require('knex');
-const connection = Knex(config);
 
-Module.Knex(connection);
+const environment = process.env.NODE_ENV || 'development';
+const config = require('../knexfile.js')[environment];
+const knex = require('knex')
+const connection = knex(config)
+
+Model.knex(connection)
 
 module.exports = require('knex')(config);
