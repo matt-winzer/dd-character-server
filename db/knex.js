4bd514a7bee0fa84a5c3a86048761b9cffd4f6d5
matt-winzer <matt.winzer@gmail.com>
2017-09-21 01:42:31 -0600

    created armor routes for get all, get one, and put

--- db/knex.js
+++ db/knex.js
@@ -1,8 +1,8 @@
-const objection = require('objection');
-const Model = objection.Model;
-
 const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile.js')[environment];
+
+const objection = require('objection');
+const Model = objection.Model;
 const knex = require('knex')
 const connection = knex(config)
 
--- routes/character.js
+++ routes/character.js
@@ -2,17 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 const Character = require('../models/Character')
+const Skill = require('../models/Skill')
+const Weapon = require('../models/Weapon')
 
 router.get('/', function(req, res, next) {
   Character
     .query()
     .eager('[player, weapons, weapons.properties, weapons.damage_type, armors, items, abilities, skills, proficiencies, spells, spells.magic_school, class, features, features.class, traits]')
-    // .modifyEager('spells.magic_school', builder => {
-    //   // Only select 'name' column of magic_school table
-    //   builder.select('name');
-    // })
+    .modifyEager('spells.magic_school', builder => {
+      // Only select 'name' column of magic_school table
+      builder.select('name');
+    })
     .then(characters => {
       res.json(characters)
+    })
+    .catch(err => {
+      console.log(err.stack)
     })
 });
 
