7952a10db42ae268f38cb93f33399ef6888aaeec
matt-winzer <matt.winzer@gmail.com>
2017-09-24 16:48:35 -0600

    refactored item, weapon, armor routes

--- routes/weapon.js
+++ routes/weapon.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const Character = require('../models/Character')
-const Skill = require('../models/Skill')
 const Weapon = require('../models/Weapon')
-
 
 router.get('/', function(req, res, next) {
   Weapon
     .query()
+    .eager('[damage_type, properties]')
     .then(weapons => {
       res.json(weapons)
     })
@@ -21,6 +19,7 @@ router.get('/:id', function(req, res, next) {
   Weapon
     .query()
     .where('id', req.params.id)
+    .eager('[damage_type, properties]')
     .then(weapon => {
       res.json(weapon)
     })
@@ -30,12 +29,10 @@ router.get('/:id', function(req, res, next) {
 });
 
 router.put('/:id', function(req, res, next) {
-  const id = req.params.id
-
   Weapon
     .query()
     .patch(req.body)
-    .where('id', id)
+    .where('id', req.params.id)
     .returning('*')
     .then(editedWeapon => {
       res.status(200).json(editedWeapon)
