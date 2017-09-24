9046983f82e376ae09a7354600afa4b086695462
matt-winzer <matt.winzer@gmail.com>
2017-09-24 16:37:18 -0600

    created character_skill route file w/get all, get one, and put routes

--- routes/character.js
+++ routes/character.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const Character = require('../models/Character')
-const Character_Ability = require('../models/Character_Ability')
-const Character_Skill = require('../models/Character_Skill')
-
-
-// CHARACTER ROUTES
 
 router.get('/', function(req, res, next) {
   Character
@@ -42,82 +37,6 @@ router.put('/:id', function(req, res, next) {
     .query()
     .patch(req.body)
     .where('id', req.params.id)
-    .returning('*')
-    .then(editedItem => {
-      res.status(200).json(editedItem)
-    })
-    .catch(err => {
-      console.log(err.stack)
-    })
-})
-
-// CHARACTER ABILITY ROUTES
-
-// router.get('/:id/ability', function(req, res, next) {
-//   Character_Ability
-//     .query()
-//     .where('character_id', req.params.id)
-//     .eager('ability')
-//     .then(abilities => {
-//       res.json(abilities)
-//     })
-// })
-//
-// router.get('/:id/ability/:abilityId', function(req, res, next) {
-//   Character_Ability
-//     .query()
-//     .where('character_id', req.params.id)
-//     .andWhere('ability_id', req.params.abilityId)
-//     .eager('ability')
-//     .then(ability => {
-//       res.json(ability)
-//     })
-// })
-//
-// router.put('/:id/ability/:abilityId', function(req, res, next) {
-//   Character_Ability
-//     .query()
-//     .patch(req.body)
-//     .where('character_id', req.params.id)
-//     .andWhere('ability_id', req.params.abilityId)
-//     .returning('*')
-//     .then(editedItem => {
-//       res.status(200).json(editedItem)
-//     })
-//     .catch(err => {
-//       console.log(err.stack)
-//     })
-// })
-
-// CHARACTER SKILL ROUTES
-
-router.get('/:id/skill', function(req, res, next) {
-  Character_Skill
-    .query()
-    .where('character_id', req.params.id)
-    .eager('[skill, skill.ability]')
-    .then(skills => {
-      res.json(skills)
-    })
-});
-
-router.get('/:id/skill/:skillId', function(req, res, next) {
-  Character_Skill
-    .query()
-    .where('character_id', req.params.id)
-    .andWhere('skill_id', req.params.skillId)
-    .eager('[skill, skill.ability]')
-    .then(skill => {
-      res.json(skill)
-    })
-});
-
-router.put('/:id/skill/:skillId', function(req, res, next) {
-  Character_Skill
-    .query()
-    .patch(req.body)
-    .where('character_id', req.params.id)
-    .andWhere('skill_id', req.params.skillId)
     .returning('*')
     .then(editedItem => {
       res.status(200).json(editedItem)
